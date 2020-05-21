f9d5a80a5830154987ce967dd85e51951cc2ab73
ttzv <tomasz.zwak@gmail.com>
2020-05-21 14:50:28 +0200

    add flatten() method to flat nested arrays

--- recursive_methods.rb
+++ recursive_methods.rb
@@ -1,11 +1,3 @@
-def test(n)
-  return "finish" if n == 0
-  test (n-1)
-  p n
-end
-
-puts test 1
-
 def factorial(n)
   return 1 if n == 0
   return 1 if n == 1
@@ -20,3 +12,24 @@ def reverse_string(string)
 end
 
 p reverse_string "recursion"
+
+def flatten(a, flat=[])
+  a.each do |el|
+    if el.is_a?(Array)
+      flatten(el, flat)
+    else
+      flat << el
+    end
+  end
+  flat
+end
+
+
+nested_ary = [6,8,9,[4,2,100,[2,[5,4,1,2],5,8],[2,4,7]],4,[5,7]]
+simplenest_ary = [1,[2,[3],2],1]
+s_n_ary = [2, 3, [1, 2]]
+
+
+p flatten s_n_ary
+p flatten simplenest_ary
+p flatten nested_ary
